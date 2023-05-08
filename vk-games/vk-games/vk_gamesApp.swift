242111b303e5a3e8ec0e914888e47482d67c2a2e
Alex777Russia <shevalex777@mail.ru>
2023-05-08 22:59:12 +0300

    init xcode project

---
+++ vk-games/vk-games/vk_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  vk_gamesApp.swift
+//  vk-games
+//
+//  Created by Алексей Шевченко on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct vk_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
